abb45c49eeca2ad060d239259a8e0a0e6fc913c3
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2016-08-30 06:59:50 +0200

    configs: Erle-Brain 2, comment ekf_att_pos_estimator

--- cmake/configs/posix_erlebrain2_release.cmake
+++ cmake/configs/posix_erlebrain2_release.cmake
@@ -42,7 +42,7 @@ set(config_module_list
 	# Estimation modules (EKF/ SO3 / other filters)
 	#
 	#modules/attitude_estimator_ekf
-	modules/ekf_att_pos_estimator
+	#modules/ekf_att_pos_estimator
 	modules/attitude_estimator_q
 	modules/position_estimator_inav
 	modules/local_position_estimator
