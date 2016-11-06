116e820e3413c8260ce815b4ae35f6ba84364c02
Victor Mayoral Vilches <v.mayoralv@gmail.com>
2016-11-07 00:01:54 +0100

    ROS: add ROS 2 external example to srcs
    Added listener/talker code to the PX4
    source code to validate ament and related
    tooling.

---
+++ src/ros/src/listener_baro.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <memory>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/float32.hpp"
+
+void chatterCallback(const std_msgs::msg::Float32::SharedPtr msg)
+{
+  std::cout << "I heard: [" << msg->data << "]" << std::endl;
+}
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("listener_baro");
+  auto sub = node->create_subscription<std_msgs::msg::Float32>(
+    "sensor_baro", chatterCallback, rmw_qos_profile_default);
+
+  rclcpp::spin(node);
+
+  return 0;
+}
