51619435929a254b64e2c6f11f4530a9e36f46b7
Víctor Mayoral Vilches <victor@erlerobot.com>
2016-11-25 12:39:52 +0100

    ROS: remove main symbol from test code

--- src/ros/example/src/listener_baro.cpp
+++ src/ros/example/src/listener_baro.cpp
@@ -10,21 +10,26 @@ void chatterCallback(const std_msgs::msg::Float32::SharedPtr msg)
   std::cout << "I heard: [" << msg->data << "]" << std::endl;
 }
 
-int main(int argc, char * argv[])
+// int main(int argc, char * argv[])
+// {
+//   rclcpp::init(argc, argv);
+//   auto node = rclcpp::Node::make_shared("listener_baro");
+//   auto sub = node->create_subscription<std_msgs::msg::Float32>(
+//     "sensor_baro", chatterCallback, rmw_qos_profile_default);
+//
+//   rclcpp::spin(node);
+//
+//   return 0;
+// }
+
+__EXPORT int listener_baro_main(int argc, char *argv[]);
+int listener_baro_main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("listener_baro");
   auto sub = node->create_subscription<std_msgs::msg::Float32>(
     "sensor_baro", chatterCallback, rmw_qos_profile_default);
 
-  rclcpp::spin(node);
-
-  return 0;
-}
-
-__EXPORT int listener_baro_main();
-int listener_baro_main()
-{
 	printf("listener_baro main\n");
 	return 0;
 }
